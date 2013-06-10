41d0200777e9af04f15e22487218c50a991aff0e
Yuriy Gromchenko <gromchen@outlook.com>
2013-06-10 15:51:00 +0100

    Project

---
+++ FallDetector/FallDetector/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main()
+{
+    cout << "Enter your favorite number:" << endl;
+    
+    int num = 0;
+    cin >> num;
+    
+    cout << "Your favorite number is " << num << endl;
+
+    return 0;
+}
