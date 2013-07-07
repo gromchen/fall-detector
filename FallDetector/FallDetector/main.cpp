51115497b3c8f77b8835d4b5192a919883d7c815
Yuriy Gromchenko <gromchen@outlook.com>
2013-07-07 14:49:10 +0100

    Edges

--- FallDetector/FallDetector/main.cpp
+++ FallDetector/FallDetector/main.cpp
@@ -13,10 +13,6 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-    Mat image;
-    Mat gray;
-    char key = 0;
-
     VideoCapture capture(0);
 
     if (!capture.isOpened())
@@ -30,15 +26,28 @@ int main(int argc, char** argv)
 
     namedWindow("Camera Video", CV_WINDOW_AUTOSIZE);
     namedWindow("Processed Video", CV_WINDOW_AUTOSIZE);
+    namedWindow("Blurred Video", CV_WINDOW_AUTOSIZE);
+    namedWindow("Canny Video", CV_WINDOW_AUTOSIZE);
+
+    Mat image;
+    Mat gray;
+    Mat blur;
+    Mat canny;
+    char key = 0;
 
     while (key != 'q')
     {
         capture >> image;
+        imshow("Camera Video", image);
 
         cvtColor(image, gray, CV_BGR2GRAY);
-
-        imshow("Camera Video", image);
         imshow("Processed Video", gray);
+
+        GaussianBlur(gray, blur, Size(5, 5), 0);
+        imshow("Blurred Video", blur);
+
+        Canny(blur, canny, 10, 100);
+        imshow("Canny Video", canny);
 
         key = waitKey(25);
     }
