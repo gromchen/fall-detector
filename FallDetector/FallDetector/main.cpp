8f834186532e82175fa95d77d53a37e2813ccb2d
Yuriy Gromchenko <gromchen@outlook.com>
2013-07-07 11:11:37 +0100

    Edge detection

--- FallDetector/FallDetector/main.cpp
+++ FallDetector/FallDetector/main.cpp
@@ -8,32 +8,25 @@
 #include <opencv2/imgproc/imgproc.hpp> // Image processing module
 
 using namespace cv;
-using namespace fd;
 
 int main(int argc, char** argv)
 {
-    Mat original_image = imread("lena.png");
-    namedWindow("Original Image", WINDOW_AUTOSIZE);
-    imshow("Original Image", original_image);
+    Mat image, gray, edge, cedge;
+    int edgeThresh = 10;
 
-    Mat hidden_image = imread("girlface.png");
-    namedWindow("Hidden Image", WINDOW_AUTOSIZE);
-    imshow("Hidden Image", hidden_image);
+    image = imread("lena.png");
+    gray.create(image.rows, image.cols, CV_8UC1);
+    cvtColor(image, gray, CV_BGR2GRAY);
 
-    Mat steged_image;
-    steganograph(original_image, hidden_image, steged_image);
-    namedWindow("Steganagraphed Image", WINDOW_AUTOSIZE);
-    imshow("Steganagraphed Image", steged_image);
+    cedge.create(gray.size(), gray.type());
 
-    Mat front_image;
-    Mat unsteged_image;
-    get_original_images(steged_image, front_image, unsteged_image);
+    Canny(gray, edge, edgeThresh, edgeThresh * 3, 3);
+    cedge = Scalar::all(0);
 
-    namedWindow("Front Image", WINDOW_AUTOSIZE);
-    imshow("Front Image", front_image);
+    image.copyTo(cedge, edge);
 
-    namedWindow("Unsteganographed Image", WINDOW_AUTOSIZE);
-    imshow("Unsteganographed Image", unsteged_image);
+    namedWindow("Output Image", CV_WINDOW_AUTOSIZE);
+    imshow("Output Image", cedge);
 
     waitKey(0);
 
