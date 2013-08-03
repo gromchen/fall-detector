75498bbb986a1f80893a889ab633a10eeb82df33
Yuriy Gromchenko <gromchen@outlook.com>
2013-08-03 13:30:26 +0100

    Comments

--- fall_detector/application.h
+++ fall_detector/application.h
@@ -1,7 +1,6 @@
 #ifndef FALL_DETECTOR_APPLICATION_H
 #define FALL_DETECTOR_APPLICATION_H
 
-
 #include "video_processor.h"
 
 namespace FallDetector
--- fall_detector/video_processor.h
+++ fall_detector/video_processor.h
@@ -1,7 +1,6 @@
 #ifndef FALL_DETECTOR_VIDEO_PROCESSOR_H
 #define FALL_DETECTOR_VIDEO_PROCESSOR_H
 
-
 #include <boost/thread/mutex.hpp>
 #include <boost/atomic.hpp>
 
@@ -9,122 +8,41 @@
 
 namespace FallDetector
 {
-    /// <summary>
-    /// Controls different aspects of the video frame processing
-    /// </summary>
     class VideoProcessor
     {
     public:
-        /// <summary>
-        /// Default constructor
-        /// </summary>
         VideoProcessor();
-
-        /// <summary>
-        /// Destructor
-        /// </summary>
         ~VideoProcessor();
 
-        /// <summary>
-        /// Sets resolution of the frame
-        /// </summary>
-        /// <param name="width">Frame width</param>
-        /// <param name="height">Frame height</param>
         void SetResolution(int width, int height);
-
-        /// <summary>
-        /// Shows or hides GUI depending on current state
-        /// </summary>
         void ShowHideGui();
-
-        /// <summary>
-        /// Gets height of bounding rectangle
-        /// </summary>
-        /// <returns>Height</returns>
-        int GetHeight(); // Obtain graph
-
-        /// <summary>
-        /// Grabs and processes the frames of the sequence
-        /// </summary>
+        int GetHeight(); // TODO: Obtain graph
         void Run();
-
-        /// <summary>
-        /// Stops run
-        /// </summary>
         void Stop();
 
     private:
-        /// <summary>
-        /// Start display frames
-        /// </summary>
         void startDisplayUI();
-
-        /// <summary>
-        /// Stop display frames
-        /// </summary>
         void stopDisplayUI();
 
-        /// <summary>
-        /// OpenCV video capture object
-        /// </summary>
         cv::VideoCapture mVideoCapture;
 
-        /// <summary>
-        /// Input display window name
-        /// </summary>
         std::string m_nameOfInputWindow;
-
-        /// <summary>
-        /// Output display window name
-        /// </summary>
         std::string m_nameOfOutputWindow;
 
-        /// <summary>
-        /// Original frame from camera
-        /// </summary>
         cv::Mat _originalFrame;
-
-        /// <summary>
-        /// One channel frame
-        /// </summary>
         cv::Mat _grayFrame;
-
-        /// <summary>
-        /// Frame with extracted foreground
-        /// </summary>
         cv::Mat _frameWithForeground;
 
-        /// <summary>
-        /// Stop the processing
-        /// </summary>
         boost::atomic<bool> m_stop;
-
-        /// <summary>
-        /// Height of bounding rectangle
-        /// </summary>
-        int _heightOfRectangle;
-
-        /// <summary>
-        /// Determine whether the user interface is shown
-        /// </summary>
         boost::atomic<bool> m_showUI;
-
-        /// <summary>
-        /// Determine whether windows of GUI were created
-        /// </summary>
-        bool m_windowsAreCreated;
-
-        /// <summary>
-        /// Frames per second
-        /// </summary>
-        double _fps;
-
-        /// <summary>
-        /// Protects video capture
-        /// </summary>
+        boost::atomic<bool> mChangeResolution;
         boost::mutex  m_mutexForVideoCapture;
 
-        boost::atomic<bool> mChangeResolution;
+        int _heightOfRectangle;
+
+        bool m_windowsAreCreated;
+        double _fps;
+
         int mWidth;
         int mHeight;
     };
